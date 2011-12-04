787327b4179ff187846e51d924ab874aab98e373
Simon Harvey <harveysimon@gmail.com>
2011-12-04 12:52:17 -0500

    Initial Commit

---
+++ FakeDepth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FakeDepth
+//
+//  Created by Simon Harvey on 11-12-04.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
